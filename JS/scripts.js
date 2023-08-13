71d3de6e42c91b53b44bb7105edf20c87fceeaea
Ralpheeze <ifeanyichukwu.eze@stu.cu.edu.ng>
2023-08-13 22:29:33 +0100

    updated

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,27 +1,27 @@
 function toggleSidebar() {
     var sidebar = document.querySelector('.sidebar');
     var sidebarList = document.querySelector(".sidebar-list");
-    var header = document.querySelector(".header");
-    var menuIcon = header.querySelector(".menu-icon");
-    var closeIcon = header.querySelector(".close-icon");
+    // var header = document.querySelector(".header");
+    // var menuIcon = header.querySelector(".menu-icon");
+    // var closeIcon = header.querySelector(".close-icon");
    
     // var closeControl = document.querySelector(".close-control");
     sidebar.classList.toggle('active');    
 
-    var sidebarWidth = parseInt(window.getComputedStyle(sidebar).width); // Get sidebar width
+    // var sidebarWidth = parseInt(window.getComputedStyle(sidebar).width); // Get sidebar width
 
     if(sidebar.classList.contains("active")) {
         sidebarList.style['display']="block";
          // menuControl.style['display']="none";
 
-        if (sidebarWidth < 141) {
-            menuIcon.style['display'] = "none";
-            // closeIcon.style['display'] = "inline-block";
-        }   else {
-                // Reset icons for larger screens if needed
-                menuIcon.style['display'] = "inline-block";
-                closeIcon.style['display'] = "none";
-        }
+        // if (sidebarWidth < 141) {
+        //     menuIcon.style['display'] = "none";
+            
+        // }   else {
+                
+        //         menuIcon.style['display'] = "inline-block";
+        //         closeIcon.style['display'] = "none";
+        // }
 }
     else {
         sidebarList.style["display"]='none';
